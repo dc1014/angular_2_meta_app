15948b8e1f25bbd8e4657a8a4a2231b8672ff126
Daniel Casper <dc@danielcasper.com>
2016-09-04 10:03:01 -0700

    Fix RC6 dependencies.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>2950 Merced</h1>'
+  template: '<h1>Hi, I am a potato!</h1>'
 })
 export class AppComponent { }
